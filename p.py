d010a4ea5d5a04c74f2647d41aad8aa323e34355
bspass <bspass17@gmail.com>
2024-05-01 22:04:37 +0800

    refactor code
    Добавил комментарии, будет релиз v0.1

--- p.py
+++ p.py
@@ -1,29 +1,37 @@
-import tkinter as tk
-import random
-import string   # проверка
+import tkinter as tk    # Импортируем модуль tkinter, который используется для создания графического интерфейса.
+import random   # Импортируем модуль random, который используется для случайных генераций.
+import string   # Импортируем модуль string, который содержит в себе буквы, цифры и спец. символы.
 
 
-def generate_password():
+def generate_password():    # Определяем функцию для генерации пароля.
+    # Получаем длину пароля из поля ввода, преобразуя ввод в целое число.
     password_length = random.randrange(1, 16)
-
+    # Инициализируем пустую строку для возможных символов пароля.
     password_characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 
+    # Генерируем пароль заданной длины, используя случайные символы из выбранных типов.
     password = ''.join(random.choice(password_characters) for _ in range(password_length))
 
-    password_entry.delete(0, tk.END)
-    password_entry.insert(0, password)
+    # Очищаем поле для отображения пароля и вставляем сгенерированный пароль.
+    password_entry.delete(0, tk.END)    # Удаляем все символы в поле.
+    password_entry.insert(0, password)  # Вставляем новый пароль.
 
 
+# Создаем главное окно приложения.
 root = tk.Tk()
-root.title("Генератор паролей")
+root.title("Генератор паролей")  # Устанавливаем заголовок окна
 
-generate_button = tk.Button(root, text="Генерировать", command=generate_password)
-generate_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+# Создаем кнопку для запуска функции генерации пароля.
+generate_button = tk.Button(root, text="Генерировать", command=generate_password)   # Кнопка запуска генерации пароля.
+generate_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)   # Размещаем кнопку в сетке.
 
-password_label = tk.Label(root, text="Сгенерированный пароль:")
-password_label.grid(row=6, column=0, columnspan=2, padx=10, pady=5)
+# Метка для отображения сгенерированного пароля.
+password_label = tk.Label(root, text="Сгенерированный пароль:")     # Метка с текстом для результата.
+password_label.grid(row=6, column=0, columnspan=2, padx=10, pady=5)     # Размещаем метку в сетке.
 
-password_entry = tk.Entry(root, width=30)
-password_entry.grid(row=7, column=0, columnspan=2, padx=10, pady=5)
+# Поле для отображения сгенерированного пароля.
+password_entry = tk.Entry(root, width=30)   # Поле ввода/вывода для пароля.
+password_entry.grid(row=7, column=0, columnspan=2, padx=10, pady=5)     # Размещаем в сетке.
 
-root.mainloop()
+# Запуск основного цикла программы, который обрабатывает события интерфейса.
+root.mainloop()  # Начинаем основной цикл, который удерживает окно открытым для взаимодействия с элементами интерфейса.
